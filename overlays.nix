fadee1d904971bbf2c16db8cee9a269b515bd213
Martin Lindbüchl <martin.lindbuechl@tum.de>
2026-06-09 14:41:25 +0000

    package tlx

--- overlays.nix
+++ overlays.nix
@@ -5,4 +5,23 @@ final: _prev: {
   osv-boost = _prev.boost181.override { enableStatic = true; };
   osv-ssl = inputs.nixpkgs-2211.legacyPackages.${_prev.system}.openssl_1_1.out;
   osv-ssl-hdr = inputs.nixpkgs-2211.legacyPackages.${_prev.system}.openssl_1_1.dev;
+  tlx = _prev.stdenv.mkDerivation rec {
+    pname = "tlx";
+    version = "0.6.1";
+
+    src = _prev.fetchFromGitHub {
+      owner = "tlx";
+      repo = "tlx";
+      rev = "v${version}";
+      hash = "sha256-qH6gHPX1GdxMKbsrKeJpKOwRdPHVjPZGIWzcOX8HxwA=";
+    };
+
+    nativeBuildInputs = with _prev; [ cmake ];
+
+    cmakeFlags = [
+      "-DCMAKE_INSTALL_LIBDIR=lib"
+      "-DCMAKE_INSTALL_INCLUDEDIR=include"
+      "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
+    ];
+  };
 }

9d5ed71cc84841718385663acf603830efe63b0f
Martin Lindbüchl <martin.lindbuechl@tum.de>
2026-06-11 08:13:57 +0000

    increase max. allowed arm-clock to support Graviton 3

--- arch/aarch64/arm-clock.cc
+++ arch/aarch64/arm-clock.cc
@@ -43,8 +43,8 @@ private:
 arm_clock::arm_clock() {
     asm volatile ("mrs %0, cntfrq_el0; isb; " : "=r"(freq_hz) :: "memory");
     /* spec documents a typical range of 1-50 MHZ,
-     * the ampere-1a however, runs on 1Ghz, so allow up to that frequency */
-    if (freq_hz < 1 * MHZ || freq_hz > 1000 * MHZ) {
+     * the ampere-1a however, runs on 1Ghz, so allow higher frequencies */
+    if (freq_hz < 1 * MHZ || freq_hz > 1500 * MHZ) {
         debug_early_u64("arm_clock(): read invalid frequency ", freq_hz);
         abort();
     }
